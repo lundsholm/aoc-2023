6fc94c63d41877b865dfd812b259945792484d9f
Jonas Musekura Lundsholm <jonas.musekura.lundsholm@devolent.com>
2023-12-09 14:32:47 +0100

    make it possible to run parts individually

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-async function runDay(day: string) {
+async function runDay(day: string, part?: string) {
 	try {
 		const dayPath = `./day-${day}`;
+		if (part) {
+			const modulePath = `${dayPath}/part-${part}`;
+			const module = await import(modulePath);
+			await module.default();
+			return;
+		}
 		for (const part of ['part-1', 'part-2']) {
 			const modulePath = `${dayPath}/${part}`;
 			const module = await import(modulePath);
@@ -13,6 +19,7 @@ async function runDay(day: string) {
 
 // check path parameter
 const day = process.argv[2];
+const part = process.argv[3];
 console.log(`advent of code 2023 day ${day}`);
 console.log();
-await runDay(day);
+await runDay(day, part);
